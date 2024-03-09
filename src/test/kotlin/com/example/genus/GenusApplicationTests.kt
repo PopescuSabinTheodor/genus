4a3036800d54a6e1696380da7a34fc3b6a5933d4
PopescuSabinTheodor <popescusabintheodor@gmail.com>
2024-03-09 20:03:43 +0200

    First commit

---
+++ src/test/kotlin/com/example/genus/GenusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.genus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GenusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
